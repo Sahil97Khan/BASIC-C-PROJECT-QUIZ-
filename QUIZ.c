0f5402ebbe886a3350bad34d79bed20d0da97913
SAHIL KHAN <110851333+Sahil97Khan@users.noreply.github.com>
2022-09-20 01:35:32 +0530

    Add files via upload

---
+++ QUIZ.c
@@ -0,0 +1,337 @@
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+#include<windows.h>
+#include<time.h>
+int main()
+
+{
+    char name[25];
+    int score=0,c=0,i=0;
+    printf("                     WELCOME TO THE QUIZ ");
+    printf("\n                       (by Sahil Khan) ");
+    printf("\n");
+
+
+    printf("PRESS ENTER TO START");
+   getch(); system("cls");
+
+
+
+    printf("\nEnter Your Name to start:-\n");
+    scanf("%s",name);
+  getch(); system("cls");
+    printf("BEST OF LUCK %s :)     ",name);
+   getch(); system("cls");
+
+
+    getch(); system("cls");
+
+    printf("Hi. %s",name);
+    printf("\nQ.1");
+    printf("\nWhat is the Capital Of India ?");
+    printf("\n      (1) MUMBAI");
+     printf("\n     (2) KOLKATA");
+     printf("\n     (3) DELHI");
+      printf("\n    (4) SHIMLA");
+      int a1;
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a1);
+      if(a1==3)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+    getch(); system("cls");
+
+    printf("Hi. %s",name);
+    printf("\nQ.2");
+    printf("\nWhat is the Capital Of RAJASTHAN ?");
+    printf("\n      (1) KOTA");
+     printf("\n     (2) JODHPUR");
+     printf("\n     (3) UDAIPUR");
+      printf("\n    (4) JAIPUR");
+      int a2;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a2);
+
+
+    getch(); system("cls");
+      if(a2==4)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+
+
+    getch(); system("cls");
+    printf("Hi. %s",name);
+    printf("\nQ.3");
+    printf("\nWhat is the Capital of West Bangal?");
+    printf("\n      (1) MUMBAI");
+     printf("\n     (2) KOLKATA");
+     printf("\n     (3) DELHI");
+      printf("\n    (4) SHIMLA");
+      int a3;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a3);
+
+
+    getch(); system("cls");
+      if(a3==2)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+
+
+    getch(); system("cls");
+    printf("Hi. %s",name);
+    printf("\nQUESTION NO. 4");
+    printf("\nName the Population Wise Largest State of India?");
+    printf("\n      (1) RAJASTHAN");
+     printf("\n     (2) UTTAR PRADESH");
+     printf("\n     (3) MADHYA PRADESH");
+      printf("\n    (4) UTTRAKHAND");
+      int a4;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a4);
+
+
+    getch(); system("cls");
+      if(a4==2)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+
+
+    getch(); system("cls");
+    printf("Hi. %s",name);
+    printf("\nQ.5");
+    printf("\nWAHT IS THE FULL FORM OF UPSC?");
+    printf("\n      (1) URBAN PUBLIC SERVICE CASE");
+     printf("\n     (2) UTTAR PRADESH SUPREME COURT ");
+     printf("\n     (3) UNDER PASS SCHOOL COMMUNITY");
+      printf("\n    (4) NONE OF THE ABOVE");
+      int a5;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a5);
+
+
+    getch(); system("cls");
+      if(a5==4)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+
+
+    getch(); system("cls");
+    printf("Hi. %s",name);
+    printf("\nQ.6");
+    printf("\nJAMIA MILLIA ISLAMIA is a ?");
+    printf("\n      (1) STATE GOVERNMENT University ");
+     printf("\n     (2) Central University");
+     printf("\n     (3) Private University");
+      printf("\n    (4) A SCHOOL");
+      int a6;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a6);
+
+
+    getch(); system("cls");
+      if(a6==2)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+
+
+
+    getch(); system("cls");
+    printf("Hi. %s",name);
+    printf("\nQ.7");
+    printf("\n PICK OUT ODD ONE ?");
+    printf("\n      (1) 476");
+     printf("\n     (2) 225");
+     printf("\n     (3) 990");
+      printf("\n    (4) 784");
+      int a7;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a7);
+
+
+    getch(); system("cls");
+      if(a7==3)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+
+
+
+    getch(); system("cls");
+    printf("Hi. %s",name);
+    printf("\nQ.8");
+    printf("\n Which One is Not a Online Shopping Plateform?");
+    printf("\n      (1) Flipkart");
+     printf("\n     (2) Amazon");
+     printf("\n     (3) Uber");
+      printf("\n    (4) Paytm");
+      int a8;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a8);
+
+
+    getch(); system("cls");
+      if(a8==3)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+
+
+
+    getch(); system("cls");
+    printf("Hi. %s",name);
+    printf("\nQ.9");
+    printf("\n How Many Centimeters(cm) are there in 1 inch ?");
+    printf("\n      (1) 3.57");
+     printf("\n     (2) 11.66");
+     printf("\n     (3) 30.28");
+      printf("\n    (4) 2.56");
+      int a9;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a7);
+
+
+    getch(); system("cls");
+      if(a9==3)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            i=i+1;
+            score=score+0;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+
+
+
+
+
+    getch(); system("cls");
+    printf("Hi. %s",name);
+    printf("\nQ.10");
+    printf("\n Who is the CEO OF GOOGLE ?");
+    printf("\n      (1) R.K NARAYAN");
+     printf("\n     (2) ALLAN MASK");
+     printf("\n     (3) SUNDER PICHAI");
+      printf("\n    (4) John W. Smith");
+      int a10;
+      //
+      printf("\nEnter Your OPTION");
+      scanf("%d",&a10);
+
+    getch(); system("cls");
+
+      if(a10==3)
+      {
+          score=score+1;
+          c=c+1;
+          printf("Congratulations...!\n Right Answer\n Score=%d/10",score);
+      }
+      else
+        {
+            score=score+0;
+            i=i+1;
+            printf("Bad One...!\n Wrong Answer\n Score=%d/10",score);
+        }
+
+    getch(); system("cls");
+    printf("                             FINAL RESULT");
+    printf("\n                               TOTAL CORRECT ANSWERS = %d",c);
+    printf("\n                               TOTAL INCORRECT ANSWERS = %d",i);
+    int t=4*c+(-1)*i;
+     printf("\n                                 TOTAL SCORE = %d",t);
+     getch();
+}
